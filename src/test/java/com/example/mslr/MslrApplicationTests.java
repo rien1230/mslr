82797c2e88668f52a1de352db085bf7bb2f04a5d
Shouqar <ns632@student.le.ac.uk>
2025-12-26 18:26:57 +0000

    Added folder structures and spring boot enviroment.

---
+++ src/test/java/com/example/mslr/MslrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mslr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MslrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
